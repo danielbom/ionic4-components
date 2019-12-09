5aba887ce926ada463e0f103172244c6ef644e79
danielbom <daniel.rodrigues.45@hotmail.com>
2019-12-09 16:55:14 -0300

    feat: Update users page

--- src/app/pages/list/users/users.page.ts
+++ src/app/pages/list/users/users.page.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.page.html',
-  styleUrls: ['./users.page.scss'],
+  selector: "app-users",
+  templateUrl: "./users.page.html",
+  styleUrls: ["./users.page.scss"]
 })
 export class UsersPage implements OnInit {
+  lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint atque laboriosam saepe eos quibusdam quos mollitia itaque odit necessitatibus dicta nam non delectus expedita magnam amet cupiditate, doloremque qui commodi?";
 
-  constructor() { }
+  users = [
+    {
+      name: "Cosima",
+      description: this.lorem,
+      avatar: "assets/img/avatar/cosima-avatar.jpg",
+      rating: 1
+    },
+    {
+      name: "Girl",
+      description: this.lorem,
+      avatar: "assets/img/avatar/girl-avatar.png",
+      rating: 2
+    },
+    {
+      name: "Ian",
+      description: this.lorem,
+      avatar: "assets/img/avatar/ian-avatar.png",
+      rating: 3
+    },
+    {
+      name: "Marty",
+      description: this.lorem,
+      avatar: "assets/img/avatar/marty-avatar.png",
+      rating: 4
+    },
+    {
+      name: "Sarah",
+      description: this.lorem,
+      avatar: "assets/img/avatar/sarah-avatar.jpg",
+      rating: 5
+    },
+  ];
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  stars(n: number) {
+    return Array.from({ length: n }).fill(0);
   }
-
 }
