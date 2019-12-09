ff3cf2ecdb891f43680692a35a89fa467124b3ae
danielbom <daniel.rodrigues.45@hotmail.com>
2019-12-09 12:10:11 -0300

    feat: Create crypto list page

---
+++ src/app/pages/list/crypto-list/crypto-list.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-list',
+  templateUrl: './crypto-list.page.html',
+  styleUrls: ['./crypto-list.page.scss'],
+})
+export class CryptoListPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
