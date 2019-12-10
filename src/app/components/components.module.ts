3583386b211cef670abaae1330e0f0edb871eb41
danielbom <daniel.rodrigues.45@hotmail.com>
2019-12-10 03:59:34 -0300

    feat: Update components module

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,13 @@
+import { AccordionListComponent } from './accordion-list/accordion-list.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { IonicModule } from '@ionic/angular';
+
+import { TimelineModule } from "./timeline/timeline.module";
+
+@NgModule({
+  declarations: [AccordionListComponent],
+  imports: [CommonModule, IonicModule],
+  exports: [TimelineModule, AccordionListComponent]
+})
+export class ComponentsModule {}
