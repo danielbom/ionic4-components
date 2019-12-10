d557719ca129903fe5a0dbb2334416cc110aba10
danielbom <daniel.rodrigues.45@hotmail.com>
2019-12-09 22:45:38 -0300

    feat: Update delete items page

--- src/app/pages/list/delete-items/delete-items.page.ts
+++ src/app/pages/list/delete-items/delete-items.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-delete-items',
-  templateUrl: './delete-items.page.html',
-  styleUrls: ['./delete-items.page.scss'],
+  selector: "app-delete-items",
+  templateUrl: "./delete-items.page.html",
+  styleUrls: ["./delete-items.page.scss"]
 })
 export class DeleteItemsPage implements OnInit {
+  items = Array.from({ length: 3 }).map((_, i) => ({ title: `Item ${i}` }));
+  things = Array.from({ length: 3 }).map((_, i) => ({ title: `Thing ${i}` }));
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  addItem() {
+    this.items.push({ title: `Item ${this.items.length + 1}` });
   }
 
+  addThing() {
+    this.things.push({ title: `Thing ${this.things.length + 1}` });
+  }
+
+  deleteItem(list: [], index) {
+    list.splice(index, 1);
+  }
 }
