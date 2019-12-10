b457cfef5a0049d9a09be288e4ede76e75274be8
danielbom <daniel.rodrigues.45@hotmail.com>
2019-12-10 03:55:59 -0300

    feat: Create timeline component (Not work)

---
+++ src/app/components/timeline/timeline.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "timeline",
+  templateUrl: "./timeline.component.html",
+  styleUrls: ["./timeline.component.scss"]
+})
+export class TimelineComponent {
+  @Input("endIcon") endIcon = "ionic";
+
+  constructor() {}
+}
+
+@Component({
+  selector: "timeline-item",
+  template: "<ng-content></ng-content>",
+})
+export class TimelineItemComponent {
+  constructor() {}
+}
+
+@Component({
+  selector:'timeline-time',
+  template: '<span>{{time.subtitle}}</span> <span>{{time.title}}</span>',
+})
+export class TimelineTimeComponent{
+  @Input() time: { title?: string, subtitle?: string} = {};
+  constructor() { }
+}
