0fa77ec151d521e546b5063b8fe906d308a3eae4
danielbom <daniel.rodrigues.45@hotmail.com>
2019-12-09 21:54:35 -0300

    feat: Update reactive list label page

--- src/app/pages/list/reactive-list-label/reactive-list-label.page.ts
+++ src/app/pages/list/reactive-list-label/reactive-list-label.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { LIST_DATA } from "./list-data";
 
 @Component({
-  selector: 'app-reactive-list-label',
-  templateUrl: './reactive-list-label.page.html',
-  styleUrls: ['./reactive-list-label.page.scss'],
+  selector: "app-reactive-list-label",
+  templateUrl: "./reactive-list-label.page.html",
+  styleUrls: ["./reactive-list-label.page.scss"]
 })
 export class ReactiveListLabelPage implements OnInit {
+  public data = LIST_DATA;
+  public dataList = [];
+  public currentCategory = "";
+  public categories = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.categories = this.data
+      .map(item => item.category)
+      .filter((item, i, array) => array.indexOf(item) === i);
+
+    this.setCategory(this.categories[0]);
   }
 
+  setCategory(category) {
+    this.currentCategory = category;
+    this.dataList = this.data.filter(
+      item => item.category === this.currentCategory
+    );
+  }
 }
